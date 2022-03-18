986c4f6f2012635b00bf28658c2460d37f781339
Lauler <fatonr@gmail.com>
2022-03-18 23:27:39 +0100

    match update. need to download html files manually after clicking 'expand models' on HF

--- get_download_stats.R
+++ get_download_stats.R
@@ -8,9 +8,9 @@ get_organization_models <- function(org_url){
     html_nodes("a") %>%
     html_attr("href")
   
-  org_model_urls <- paste0("https://huggingface.co", organization_models)
+  # org_model_urls <- paste0("https://huggingface.co", organization_models)
   
-  return(org_model_urls)
+  return(organization_models)
 }
 
 get_download_stats <- function(model_url){
@@ -25,8 +25,8 @@ get_download_stats <- function(model_url){
               "downloads" = as.numeric(downloads)))
 }
 
-kblab_models <- get_organization_models("https://huggingface.co/KBLab")
-kb_models <- get_organization_models("https://huggingface.co/KB")
+kblab_models <- get_organization_models("KBLab.html") # https://huggingface.co/KBLab 
+kb_models <- get_organization_models("KB.html") # https://huggingface.co/KBLab
 
 df_kblab <- purrr::map_df(kblab_models, get_download_stats)
 df_kb <- purrr::map_df(kb_models, get_download_stats)
--- plot_stats.R
+++ plot_stats.R
@@ -7,45 +7,79 @@ data_files <- list.files("data", full.names = TRUE)
 
 df <- data_files %>%
   purrr::map_df(~readr::read_csv(.))
+
+
+df_total_sum <- df %>% 
+  group_by(date) %>%
+  summarize(downloads = sum(downloads))
   
+p_dl_total <- ggplot(data = df_total_sum, 
+              aes(x = date, y = downloads)) +
+  geom_line(colour = "firebrick2") +
+  geom_point(shape = 21, size = 1.5, colour = "black", fill = "firebrick2") + 
+  expand_limits(y = 0) +
+  scale_y_continuous(breaks = seq(0, max(df_total_sum$downloads), by = 5000)) +
+  scale_x_date(date_labels = "%b %d", breaks = unique(df_model_top$date)) +
+  theme_light(base_size = 5) +
+  labs(y = "Number of downloads",
+       x = "Date",
+       title = "Total number of downloads per month")
+
+
+
+
 df_sum <- df %>%
   group_by(organization, date) %>%
   summarize(downloads = sum(downloads))
 
-df_model <- df %>%
-  group_by(model_name, date) %>%
-  summarize(downloads = sum(downloads))
-
-
 p_dl_org <- ggplot(data = df_sum, 
                       aes(x = date, y = downloads, fill = organization)) +
-  geom_point(shape = 21, size = 4, colour = "black") + 
+  geom_line(aes(color = organization)) +
+  geom_point(shape = 21, size = 1.5, colour = "black") + 
   expand_limits(y = 0) +
   scale_y_continuous(breaks = seq(0, max(df_sum$downloads) + 1000, by = 5000)) +
-  theme_light(base_size = 8) +
+  scale_x_date(date_labels = "%b %d", breaks = unique(df_model_top$date)) +
+  theme_light(base_size = 5) +
   labs(y = "Number of downloads",
        x = "Date",
        title = "Total number of downloads by organization",
-       fill = "Organization")
+       fill = "Organization") +
+  guides(color = "none")
 
 
-df_model_top <- df_model %>%
+df_model <- df %>%
+  group_by(model_name) %>%
+  summarize(downloads = sum(downloads)) %>%
   arrange(-downloads) %>%
   ungroup %>%
-  slice_max(downloads, n = 10)
+  slice_max(downloads, n = 10) 
+
+# Models with top 10 most downloads over entire period
+df_model_top <- df[df$model_name %in% df_model$model_name, ]
 
 
 p_dl_model <- ggplot(data = df_model_top, 
-       aes(x = date, y = downloads, fill = fct_inorder(model_name))) +
-  geom_point(shape = 21, size = 4, colour = "black") +
-  theme_light(base_size = 8) +
+                     aes(x = date, y = downloads, fill = fct_reorder(model_name, desc(downloads)))) +
+  geom_line(aes(color = fct_reorder(model_name, desc(downloads)))) +
+  geom_point(shape = 21, size = 1.5, colour = "black") +
+  theme_light(base_size = 5) +
   scale_y_continuous(breaks = seq(0, max(df_model_top$downloads) + 1000, by = 5000)) +
+  scale_x_date(date_labels = "%b %d", breaks = unique(df_model_top$date)) +
   expand_limits(y = 0) +
   labs(y = "Number of downloads",
        x = "Date",
-       title = "Total number of downloads for top 10 models",
-       fill = "Organization")
+       title = "Number of downloads by model for top 10 models",
+       fill = "Model") +
+  guides(color = "none")
 
+
+ggsave(p_dl_total, 
+       filename = "plots/downloads_total.jpg", 
+       dpi = 300, 
+       type = "cairo",
+       width = 1920, 
+       height = 1080,
+       units = "px")
 
 ggsave(p_dl_org, 
        filename = "plots/downloads_by_org.jpg", 
